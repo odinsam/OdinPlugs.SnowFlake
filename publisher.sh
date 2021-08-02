7cbc93b3fad4175fae5c3f1b9a2e85b258a339f3
‘odinsam’ <123679@qq.com>
2021-08-02 18:30:21 +0800

    📃 docs(*): publish.sh

--- publisher.sh
+++ publisher.sh
@@ -1,3 +1,4 @@
 dotnet pack --configuration Release
 dotnet nuget push "bin/Release/OdinPlugs.SnowFlake.1.0.5.nupkg"  --api-key "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" --source "github"
 dotnet nuget add source --username odinsam --password "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" --store-password-in-clear-text --name github "https://nuget.pkg.github.com/odinsam/index.json"
+
